0fe608dec4ddf0f9c7ef2e6623934fb59beaad0d
James Morris <hey@jamesmorris.co>
2020-01-17 23:23:57 +0000

    fixed misconfiguration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({});
-module.exports = {
-  head: {
-    // this htmlAttrs you need
-    htmlAttrs: {
-      lang: 'en',
-    },
-  }
-}
